7f475c1696f56e36c682da90c058807cf3c8ca7d
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2025-06-19 14:29:06 -0700

    initial commit: mob band website app

---
+++ src/main/java/com/carlosescamilla/mob/band_website/BandWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.carlosescamilla.mob.band_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlosescamilla/mob/band_website/BandWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosescamilla.mob.band_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
